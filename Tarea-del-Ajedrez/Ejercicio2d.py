916c845fecc957e8034f931c46ddd8638477f9b4
andre98652 <132710386+andre98652@users.noreply.github.com>
2023-06-05 20:15:43 -0500

    ejercicio4

--- Tarea-del-Ajedrez/Ejercicio2d.py
+++ Tarea-del-Ajedrez/Ejercicio2d.py
@@ -1,2 +1,12 @@
 from interpreter import draw
 from chessPictures import *
+
+square_white = Picture(SQUARE)
+square_black = square_white.changeColor('_', '#')
+
+square_block = square_white.join(square_black)
+
+for i in range(6):
+    square_block = square_block.join(square_white if i % 2 == 0 else square_black)
+
+draw(square_block)
