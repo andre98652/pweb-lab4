6aed56b7378aa366f8247b8bd075e04417d4b2ab
andre98652 <132710386+andre98652@users.noreply.github.com>
2023-06-05 19:52:26 -0500

    ejercicio2

--- Tarea-del-Ajedrez/Ejercicio2b.py
+++ Tarea-del-Ajedrez/Ejercicio2b.py
@@ -1,2 +1,19 @@
+from pieces import *
+from picture import *
 from interpreter import draw
-from chessPictures import *
+from colors import *
+
+# Crear una instancia de la clase Picture con el caballo blanco
+horse_white = Picture(KNIGHT)
+
+
+horse_black = horse_white.changeColor('.', '@')
+horse_combined = horse_black.join(horse_white)
+
+horse_white2 = Picture(KNIGHT)
+horse_black2 = horse_white.changeColor('.', '@')
+horse_combined2 = horse_black2.join(horse_white2)
+horse_combined2 = horse_combined2.horizontalMirror() 
+horse_block=horse_combined.under(horse_combined2)
+
+draw(horse_block)
