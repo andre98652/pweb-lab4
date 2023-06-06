54f8fbbdcd25e091065a117fd4c680adde4007b6
andre98652 <132710386+andre98652@users.noreply.github.com>
2023-06-05 19:52:44 -0500

    ejercicio3

--- Tarea-del-Ajedrez/Ejercicio2c.py
+++ Tarea-del-Ajedrez/Ejercicio2c.py
@@ -1,2 +1,13 @@
+from pieces import *
+from picture import *
 from interpreter import draw
-from chessPictures import *
+from colors import *
+
+# Crear una instancia de la clase Picture con el caballo blanco
+queen_white = Picture(QUEEN)
+queen_block = Picture(QUEEN)
+
+for i in range(3):
+    queen_block=queen_block.join(queen_white)
+
+draw(queen_block)
